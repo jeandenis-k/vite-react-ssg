f6e6a28d7f4b525d57b669c73f2ad6b1f4b58ce1
Jean-Denis Koeck <jdkoeck@gmail.com>
2022-05-23 10:44:14 +0200

    Rename out dir for ssg entrypoint

--- build.ts
+++ build.ts
@@ -5,7 +5,7 @@ async function main() {
   const template = await readFile("./dist/static/index.html", {
     encoding: "utf8",
   });
-  const { render } = require("./dist/server/entry-ssg.js");
+  const { render } = require("./dist/ssg/entry-ssg.js");
 
   for (const file of await readdir("./src/pages")) {
     const name = file.replace(/\.tsx$/, "").toLowerCase();
