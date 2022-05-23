b16578df248f5d43ee62f7f9a7cf4c6d8b43417c
Jean-Denis Koeck <jdkoeck@gmail.com>
2022-05-23 10:43:20 +0200

    Rename entry script

--- build.ts
+++ build.ts
@@ -5,7 +5,7 @@ async function main() {
   const template = await readFile("./dist/static/index.html", {
     encoding: "utf8",
   });
-  const { render } = require("./dist/server/entry-server.js");
+  const { render } = require("./dist/server/entry-ssg.js");
 
   for (const file of await readdir("./src/pages")) {
     const name = file.replace(/\.tsx$/, "").toLowerCase();
